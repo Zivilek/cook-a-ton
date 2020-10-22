7f7d6fffa65295a44a59a9ca3bb79696ed7d3e34
zivilekin <zivilek@wix.com>
2020-10-22 20:38:56 +0300

    recipe widget template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import RecipeWidget from './components/recipe-widget';
+
+const recipe = {
+    pictureUrl: 'https://prod-wolt-venue-images-cdn.wolt.com/5b2a9c77e4d0ef000d2bbe40/f091ab5fe619d82e32510df20211dfc5',
+    title: 'Pica su pievagrybiais'
+}
 
 const App = () => {
     return (
-        <div>Hello cook-a-ton</div>
+        <div>
+            <RecipeWidget 
+                picture = {recipe.pictureUrl}
+                title = {recipe.title}
+            />
+            <RecipeWidget 
+                picture = {recipe.pictureUrl}
+                title = {recipe.title}
+            />
+            <RecipeWidget 
+                picture = {recipe.pictureUrl}
+                title = {recipe.title}
+            />
+            <RecipeWidget 
+                picture = {recipe.pictureUrl}
+                title = {recipe.title}
+            />
+            <RecipeWidget 
+                picture = {recipe.pictureUrl}
+                title = {recipe.title}
+            />
+            <RecipeWidget 
+                picture = {recipe.pictureUrl}
+                title = {recipe.title}
+            />
+            
+        </div>
     );
 }
 
