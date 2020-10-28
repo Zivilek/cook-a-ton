a39b43f50a4074b83b8eef1eb80849c02d552340
ltuigs <ignaspovilauskas@gmail.com>
2020-10-28 20:10:55 +0200

    add request logging

--- cook-a-ton.api/src/config/server.js
+++ cook-a-ton.api/src/config/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import setRoutes from "./routes";
+import morgan from "morgan";
+const logPatter = ":method :url :status :response-time ms"
 const server = express();
 
 server.use(bodyParser.json());
+server.use(morgan(logPatter));
 
 var router = express.Router();
 setRoutes(router);
--- cook-a-ton.api/src/index.js
+++ cook-a-ton.api/src/index.js
@@ -1,9 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import './config/database';
 import server from './config/server';
 
 const PORT = process.env.PORT || 3004;
+
 server.listen(PORT, () => {
     console.log(`app running on port ${PORT}`);
 });
