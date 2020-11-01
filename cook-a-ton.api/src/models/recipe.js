2d8f5d89c9aa517a79e84cc01e1b8198e419401d
zivilekin <zivilek@wix.com>
2020-11-01 21:04:20 +0200

    tingiu rasyt ka dariau sry

--- cook-a-ton.api/src/models/recipe.js
+++ cook-a-ton.api/src/models/recipe.js
@@ -2,35 +2,40 @@ import mongoose, { Schema } from "mongoose";
 import BaseModel from "./baseModel";
 
 class Recipe extends BaseModel {
-    constructor() {
-        super('recipe');
-    }
+  constructor() {
+    super("recipe");
+  }
 
-    createSchema() {
-        const schema = new Schema({
-            name: {
-                type: String,
-                required: true,
-            },
-            description: {
-                type: String,
-                required: false,
-            },
-            course: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'userCourse',
-            },
-            tags: [{
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'userRecipeTag'
-            }],
-            images: [{
-                type: Buffer
-            }]
-        });
+  createSchema() {
+    const schema = new Schema({
+      name: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: false,
+      },
+      course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "userCourse",
+      },
+      tags: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "userRecipeTag",
+        },
+      ],
+      images: [
+        {
+          // type: Buffer
+          type: String,
+        },
+      ],
+    });
 
-        return schema;
-    }
+    return schema;
+  }
 }
 
 export default Recipe;
