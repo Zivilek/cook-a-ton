2a890f8821053a5b44e3e5ccbe82608ce123d59a
zivilekin <zivilek@wix.com>
2020-11-01 19:57:46 +0200

    added Ignas api

--- cook-a-ton.api/src/config/server.js
+++ cook-a-ton.api/src/config/server.js
@@ -2,13 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import configureRoutes from "./routes";
 import _ from "express-async-errors";
+import cors from "cors";
 import error from "../middlewares/errorHandlingMiddleware";
 import routeNotFound from "../middlewares/routeNotFoundMiddleware";
-import requestLogging from "../middlewares/requestLoggingMiddleware"
+import requestLogging from "../middlewares/requestLoggingMiddleware";
 const server = express();
 
 server.use(bodyParser.json());
 server.use(requestLogging);
+server.use(cors());
 
 configureRoutes(server);
 
